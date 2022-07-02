9e828ee408e9ef05166ccc3d848cee438d1a0c6b
AnrryPetrin <anrry.petrin@hotmail.com>
2022-07-02 15:36:37 -0300

    Teste da API da Petstore, da entidade Pet com os metodos get e put

--- tests/api/test_pet.py
+++ tests/api/test_pet.py
@@ -1,4 +1,6 @@
 # bibliotecas
+import json
+
 import requests
 
 # variaveis publicas
@@ -31,6 +33,36 @@ def teste_incluir_pet():
     corpo_do_resultado_obtido = resultado_obtido.json()
     print(corpo_do_resultado_obtido)
 
+    # valida
+    assert resultado_obtido.status_code == status_code_esperado
+    assert corpo_do_resultado_obtido['id'] == pet_id_esperado
+    assert corpo_do_resultado_obtido['name'] == pet_nome_esperado
+    assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
+    assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_nome_tag_esperado
+
+
+def teste_consultar_pet():
+    # configura
+    # entrada
+    pet_id = '5189529'
+
+    # resuldados esperados
+    status_code_esperado = 200
+    pet_id_esperado = 5189529
+    pet_nome_esperado = "Hex"
+    pet_nome_categoria_esperado = "cachorro"
+    pet_nome_tag_esperado = "vacinado"
+
+    # executa
+    resultado_obtido = requests.get(
+        url=url + '/' + pet_id,
+        headers=headers
+    )
+
+    # retorno
+    print(resultado_obtido)
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(json.dumps(corpo_do_resultado_obtido, indent=4))
 
     # valida
     assert resultado_obtido.status_code == status_code_esperado
@@ -38,3 +70,35 @@ def teste_incluir_pet():
     assert corpo_do_resultado_obtido['name'] == pet_nome_esperado
     assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
     assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_nome_tag_esperado
+
+
+def teste_alterar_pet():
+    # configure
+    # dados de entrada virão do pet2.json
+
+    # resultados esperados
+    status_code_esperado = 200
+    pet_id_esperado = 5189529
+    pet_nome_esperado = "Hex"
+    pet_nome_categoria_esperado = "cachorro"
+    pet_nome_tag_esperado = "vacinado"
+    pet_status_esperado = "pending"
+    # executa
+    resultado_obtido = requests.put(
+        url=url,
+        headers=headers,
+        data=open('C:\\Users\\anrry\\PycharmProjects\\134inicial\\vendors\\json\\pet2.json')
+    )
+
+    # retorno
+    print(resultado_obtido)
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(json.dumps(corpo_do_resultado_obtido, indent=4))
+
+    # valida
+    assert resultado_obtido.status_code == status_code_esperado
+    assert corpo_do_resultado_obtido['id'] == pet_id_esperado
+    assert corpo_do_resultado_obtido['name'] == pet_nome_esperado
+    assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
+    assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_nome_tag_esperado
+    assert corpo_do_resultado_obtido['status'] == pet_status_esperado
