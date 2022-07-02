c9ac4645516a71e261596f430bd160026d6cf00c
AnrryPetrin <anrry.petrin@hotmail.com>
2022-07-01 23:02:24 -0300

    Teste da API da Petstoree, da entidade Pet com o metodo Post

---
+++ tests/api/test_pet.py
@@ -0,0 +1,39 @@
+# bibliotecas
+import requests
+
+# variaveis publicas
+url = 'https://petstore.swagger.io/v2/pet'
+headers = {'Content-Type': 'application/json'}
+
+
+# definições das funções (defs)
+
+def teste_incluir_pet():
+    # configura
+    # dados de entrada provem do pet1.json
+
+    # resultados esperados
+    status_code_esperado = 200
+    pet_id_esperado = 5189529
+    pet_nome_esperado = "Hex"
+    pet_nome_categoria_esperado = "cachorro"
+    pet_nome_tag_esperado = "vacinado"
+
+    # executa
+    resultado_obtido = requests.post(
+        url=url,
+        headers=headers,
+        data=open('C:\\Users\\anrry\\PycharmProjects\\134inicial\\vendors\\json\\pet1.json')
+    )
+
+    # retorno
+    print(resultado_obtido)
+    corpo_do_resultado_obtido = resultado_obtido.json()
+    print(corpo_do_resultado_obtido)
+
+    # valida
+    assert resultado_obtido.status_code == status_code_esperado
+    assert corpo_do_resultado_obtido['id'] == pet_id_esperado
+    assert corpo_do_resultado_obtido['name'] == pet_nome_esperado
+    assert corpo_do_resultado_obtido['category']['name'] == pet_nome_categoria_esperado
+    assert corpo_do_resultado_obtido['tags'][0]['name'] == pet_nome_tag_esperado
