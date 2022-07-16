1a08df32d122f6884e96ead145d7ffd90b28b955
AnrryPetrin <anrry.petrin@hotmail.com>
2022-07-16 20:12:48 -0300

    Automatizado o fluxo na pagina home

--- tests/web/selenium_webdriver.py
+++ tests/web/selenium_webdriver.py
@@ -1,7 +1,6 @@
 # configura
 # bibliotecas / imports
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 # dados de entrada
@@ -16,6 +15,7 @@ titulo_passagens_esperado = 'Flights from São Paolo to Buenos Aires:'
 mensagem_agradecimentp_esperada = 'Thank you for your purchase today!'
 preco_passagem_esperado = '555 USD'
 
+
 # executa
 class Testes:
     # inicio
@@ -23,7 +23,6 @@ class Testes:
         # instanciar a biblioteca / motor / engine
         # informar onde esta o WebDriver
         self.driver = webdriver.Edge('../../vendors/drivers/msedgedriver103.0.1264.49.exe')
-        self.vars = {}
 
     # fim
     def teardown_method(self):
@@ -35,6 +34,21 @@ class Testes:
         # e2e / end to end / ponta a ponta
         # pagina inicial (home)
         # executa / valida
+        # abrir o browser no endereco
+        self.driver.get("https://www.blazedemo.com")
+        # clicar na lista de cidades de origem
+        lista = self.driver.find_element(By.NAME, "fromPort")
+        lista.click()
+        # selecionar a cidade desejada
+        lista.find_element(By.XPATH, "//option[. = 'São Paolo']").click()
+        # clicar na lista de cidades de origem
+        lista = self.driver.find_element(By.NAME, "toPort")
+        lista.click()
+        # selecionar a cidade de destino
+        lista.find_element(By.XPATH, "//option[. = 'New York']")
+        # clicar no botao de procurar voos
+        self.driver.find_element(By.CSS_SELECTOR, "input.btn.btn-primary")
+
 
         # pagina lista de passagens
         # executa / valida
